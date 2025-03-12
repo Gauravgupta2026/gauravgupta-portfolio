856407e0878a1130b297bdcea33ad244121579e1
Gaurav Gupta <gaurav.mitmpl2022@learner.manipal.edu>
2025-03-13 00:53:47 +0530

    refines

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,7 +5,6 @@ import Header from '../../components/Header/Header';
 import { Divider } from '@nextui-org/react';
 import Preview from '../../components/footer/scroll_Footer';
 import MediaBetweenTextScrollDemo from '../../components/Projects/newProjectCard';
-import Notes from '../../components/Notes/Notes';
 import NewNotes from '../../components/Notes/newNotes';
 
 const Home: React.FC = () => {
